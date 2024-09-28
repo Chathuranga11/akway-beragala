e4777ae25d2610a09edf99f4650fcc04c6e46a07
Chathuranga11 <cdagunarathna@gmail.com>
2024-09-28 16:59:25 +0530

    commit
    commit

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,27 @@ Route::get('/rooms', function () {
 Route::get('/contact', function () {
     return view('hotel.contact');
 });
+
+Route::get('/', function () {
+    return view('hotel.index');
+});
+
+Route::get('/rooms', function () {
+    return view('hotel.rooms');
+});
+
+Route::get('/restaurant', function () {
+    return view('hotel.restaurant');
+});
+
+Route::get('/about', function () {
+    return view('hotel.about');
+});
+
+Route::get('/blog', function () {
+    return view('hotel.blog');
+});
+
+Route::get('/contact', function () {
+    return view('hotel.contact');
+});
